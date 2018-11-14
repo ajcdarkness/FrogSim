e7e971af181bc3b4e1a925280bb70b2a2f68b2f3
Jacob Cedar <jcedar8640@bths.edu>
2018-11-14 10:35:36 -0500

    added class structure

---
+++ src/com/company/FrogSimulation.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class FrogSimulation {
+    private int goalDistance;
+    private int maxHops;
+
+    public FrogSimulation(int dist, int numHops){
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+    private int hopDistance(){
+
+    }
+    public boolean simulate(){
+
+    }
+    public double runSimulations(int num){
+
+    }
+
+}
